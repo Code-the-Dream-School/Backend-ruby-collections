a54d82a77e3a9d7ae228c99a23b6883c1e3ee3b1
ARSmith-prog <55244590+ARSmith-prog@users.noreply.github.com>
2021-04-08 05:32:37 -0400

    Update add_up.rb
    updated answer

--- add_up.rb
+++ add_up.rb
@@ -1,20 +1,17 @@
 # Alannah Smith
 # 3/31/21
 
-def add_up()
+def add_up(i)
   puts "Enter a positive number"
-  mo = gets.chomp.to_i
-
-   if( mo % 2 == 0 )
-      x =0.upto(mo).inject{|memo, i| memo +=i; memo}
-      puts x
-   else
-     puts "Oops, we needed a positive number"
-
-end
+    mo = gets.chomp.to_i
+       if( mo % 2 == 0 )
+          x =0.upto(mo).inject{|memo, i| memo +=i; memo}
+          puts x
+       else
+         puts "Oops, we needed a positive number"
+     end
 end
 
-
-add_up()
-add_up()
-add_up()
+add_up(2)
+add_up(3)
+add_up(4)

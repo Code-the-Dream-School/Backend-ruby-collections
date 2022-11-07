2b7aa9ba6a550568000dd5bf722dceb0c30ad536
Sheree1986 <sheree.edmund@gmail.com>
2022-11-07 18:00:09 -0500

    fullname complete

---
+++ full_name.rb
@@ -0,0 +1,17 @@
+name = []
+
+puts "Please enter your first name?"
+first_name = gets.chomp
+name << first_name
+
+puts "Please enter your name?"
+middle_name = gets.chomp
+name << middle_name
+
+puts "Please enter your last name?"
+last_name = gets.chomp
+name << last_name
+
+print "Hello, "
+name.each {|item| print item + " "} 
+print "!\n"
